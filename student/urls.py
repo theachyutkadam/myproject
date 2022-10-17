8e46c7bc71534e0b9ba7a352e297111134ef0aad
achyut kadam <achyut.k@acldigital.com>
2022-10-17 16:07:41 +0530

    show update record and delete record

--- student/urls.py
+++ student/urls.py
@@ -5,13 +5,13 @@ from student import views
 urlpatterns = [
   path('admin/', admin.site.urls),
   path('index', views.index, name="index-page"),
-  path('new', views.new),
-  path('create', views.create),
+  path('new', views.new , name="new-page"),
+  path('create', views.create , name="create-page"),
 
-  path('show/<int:id>', views.show),
-  path('edit/<int:id>', views.edit),
+  path('show/<int:id>', views.show , name="show-page"),
+  path('edit/<int:id>', views.edit , name="edit-page"),
 
-  path('update/<int:id>', views.update),
-  path('delete/<int:id>', views.destroy),
+  path('update/<int:id>', views.update , name="update-page"),
+  path('delete/<int:id>', views.destroy , name="destroy-page"),
 ]
 
--- student/views.py
+++ student/views.py
@@ -5,44 +5,44 @@ from .models import Student
 # Create your views here.
 def index(request):
   students = Student.objects.all()
+  print("++++++++++++++++++D")
+  print(students)
+  print("++++++++++++++++++D")
   return render(request, "student/index.html", {'students': students})
 
 def new(request):
   return render(request, "student/new.html")
 
 def show(request, id):
-  student = find_student(id)
+  student = find_student(request, id)
   # student = Student.objects.get(id=id)
   return render(request, "student/show.html", {'student': student})
 
 def edit(request, id):
-  student = find_student(id)
+  student = find_student(request, id)
   # student = Student.objects.get(id=id)
-  return render(request, "student/show.html", {'student': student})
+  return render(request, "student/edit.html", {'student': student})
 
 def destroy(request, id):
-  student = find_student(id)
+  student = find_student(request, id)
   # student = Student.objects.get(id=id)
   student.delete()
-  return redirect("/index")
+  return redirect("student/index/")
 
 def find_student(request, id):
   student = Student.objects.get(id=id)
   return student
 
 def create(request):
-  print("______________++++++++++++")
-  print(request.method)
+  print("+++++++++++++++++")
+  print(request)
+  print("+++++++++++++++++")
   if request.method == "POST":
     form = StudentForm(request.POST)
-    print("++++++++++++")
-    print(form.is_valid())
-    breakpoint()
-    print("++++++++++++")
     if form.is_valid():
       try:
         form.save()
-        return redirect('/show')
+        return redirect('show')
       except:
         pass
   else:
@@ -50,10 +50,16 @@ def create(request):
   return render(request, 'student/index.html', {'form': form})
 
 def update(request, id):
-  student = find_student(id)
+  student = find_student(request, id)
   # student = Student.objects.get(id=id)
+
   form = StudentForm(request.POST, instance = student)
+  print("+++++++++++++++++++++++")
+  print(student)
+  print(form.is_valid())
+  print(form)
+  print("+++++++++++++++++++++++")
   if form.is_valid():
     form.save()
-    return redirect("/show")
+    return redirect("show")
   return render(request, 'student/edit.html', {'student': student})
