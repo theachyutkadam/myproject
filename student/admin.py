445306943272de0b9dfdd2c544287c848e5827b7
achyut kadam <achyut.k@acldigital.com>
2022-10-17 16:22:29 +0530

    show add data on admin panel

--- student/admin.py
+++ student/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Student
+
+class StudentAdmin(admin.ModelAdmin):
+  list_display = ('id', 'contact', 'address', 'is_active', 'birth_date')
 
 # Register your models here.
+admin.site.register(Student, StudentAdmin)
